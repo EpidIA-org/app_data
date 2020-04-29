b6a054c19de1916e92c196bdd721da5a87ff8c4a
wbenbihi <waligoo@gmail.com>
2020-04-29 21:53:38 +0200

    [ADD] Weekly ingestion

--- _libs/__init__.py
+++ _libs/__init__.py
@@ -1,2 +1,3 @@
 from _libs.azure_blob_connector import AzureBlobConnector
 from _libs.scrapper_datagouv import DataGouvScrapper, DataGouvSOSMedecinScrapper, DataGouvTestCovidScrapper
+from _libs.scrapper_insee import InseeScrapper
