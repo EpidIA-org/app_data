428b703893eb08b7ec61488be657c4b209c78d5e
wbenbihi <waligoo@gmail.com>
2020-05-03 00:57:00 +0200

    [FIX] cron job bindings

--- cron_daily/__init__.py
+++ cron_daily/__init__.py
@@ -1,6 +1,5 @@
 import azure.functions as func
 from urllib.request import urlopen
 
-def main(timer: func.TimerRequest) -> func.HttpResponse:
+def main(timer: func.TimerRequest) -> None:
     resp = urlopen('https://covid-ia-fileoperations.azurewebsites.net/api/ingestion_daily')
-    return func.HttpResponse("Daily Cron Called")
--- cron_weekly/__init__.py
+++ cron_weekly/__init__.py
@@ -1,6 +1,5 @@
 import azure.functions as func
 from urllib.request import urlopen
 
-def main(timer: func.TimerRequest) -> func.HttpResponse:
+def main(timer: func.TimerRequest) -> None:
     resp = urlopen('https://covid-ia-fileoperations.azurewebsites.net/api/ingestion_weekly')
-    return func.HttpResponse("Weekly Cron Called")
